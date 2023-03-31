24e93d31ddc50593db968aa64d8f895693024e67
joshjh2002 <joshhelman@hotmail.co.uk>
2023-03-31 22:14:54 +0100

    Removed stylesheets

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import styles from "@/styles/Home.module.css";
 import Navbar from "../partials/Navbar";
 import About from "../partials/About";
 import WhatDoWeOffer from "../partials/WhatDoWeOffer";
@@ -15,7 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main>
         <Navbar />
         <About />
         <WhatDoWeOffer />
