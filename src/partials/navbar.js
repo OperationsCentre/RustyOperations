7077366b15e7b8d330241b1eb4fbc369e5ef7c1f
joshjh2002 <joshhelman@hotmail.co.uk>
2023-03-31 21:51:35 +0100

    Added navbar file

---
+++ src/partials/navbar.js
@@ -0,0 +1,15 @@
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function navbar() {
+  return (
+    <>
+      <h1>Rusty Operations</h1>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+      </ul>
+    </>
+  );
+}
