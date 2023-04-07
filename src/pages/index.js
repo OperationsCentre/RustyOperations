5d92df1459505b514dd6e020e76ebf180db8e75e
joshjh2002 <joshhelman@hotmail.co.uk>
2023-04-07 14:52:34 +0100

    Styled navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,8 +15,8 @@ export default function Home() {
         <link rel="icon" href="./favicon.ico" />
       </Head>
       <main>
+        <Navbar />
         <div className="content">
-          <Navbar />
           <About />
           <WhatDoWeOffer />
           <Shop />
--- src/partials/navbar.js
+++ src/partials/navbar.js
@@ -1,10 +1,23 @@
 export default function Navbar() {
   return (
-    <section id="navbar">
-      <h1>Rusty Operations</h1>
+    <section id="navbar" className="navbar">
+      <h1 className="website-name-font">Rusty Operations</h1>
       <ul>
-        <li>1</li>
-        <li>2</li>
+        <li>
+          <a className="active">Home</a>
+        </li>
+        <li>
+          <a>About Us</a>
+        </li>
+        <li>
+          <a>What We Offer</a>
+        </li>
+        <li>
+          <a>Shop</a>
+        </li>
+        <li>
+          <a>Social</a>
+        </li>
       </ul>
     </section>
   );
